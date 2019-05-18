59828b5e1f95e0f2ac10382df6ddd2957b09b563
gaston oliva <gastonroliva@gmail.com>
2019-05-18 10:31:24 -0300

    sprint1

---
+++ Login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <div class="dropdown-menu">
+  <form class="px-4 py-3">
+    <div class="form-group">
+      <label for="exampleDropdownFormEmail1">Email</label>
+      <input type="email" class="form-control" id="exampleDropdownFormEmail1" placeholder="email@ejemplo.com">
+    </div>
+    <div class="form-group">
+      <label for="exampleDropdownFormPassword1">Contraseña</label>
+      <input type="password" class="form-control" id="exampleDropdownFormPassword1" placeholder="Contraseña">
+    </div>
+    <div class="form-group">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="dropdownCheck">
+        <label class="form-check-label" for="dropdownCheck">
+          Recordarme
+        </label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Log in</button>
+  </form>
+  <div class="dropdown-divider"></div>
+  <a class="dropdown-item" href="#">¿Acaso eres nuevo? Registrate</a>
+  <a class="dropdown-item" href="#">¿Olvido su contraseña?</a>
+</div>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ section.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+  </head>
+  <section class="productos">
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+    <article class="producto">
+      <div class="foto">
+        <img class="foto" src="imagen.jpg" alt="algo">
+      </div>
+      <h2>titulo foto</h2>
+      <p>desc producto</p>
+      <a class="mas" href="#">ver más</a>
+    </article>
+  </section>
+</html>
